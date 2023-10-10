8ba65e1f5bb1c7602e6ef573279078516acea9f8
shahriyor2 <shahriyor.asadov.03@mail.ru>
2023-10-10 18:23:38 +0500

    forBegin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoaidung] = React.useState(true)
+
+  React.useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data)
+      }).finally(() => setIsLoaidung(false));
+  },[]);
+
   return (
     <div className="App">
-      <Users />
+      <Users items={users} isLoading={isLoading}/>
       {/* <Success /> */}
     </div>
   );
